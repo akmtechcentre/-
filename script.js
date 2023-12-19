1c9d960b1c0bf7dc0ca2de45927f57d81ad6bb76
Your Name <you@example.com>
2023-12-19 14:27:39 -0800

    i

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ const Load = ()=>{
    preload.style.display="none";
    mainbody.style.display="block";
 	}, timer)
+
+
+
+
+var topBanner=["img/team.jpg",
+	"img/events-1.jpg",
+	"img/course-1.jpg"];
+
+var random=Math.floor(Math.random() * topBanner.length);
+
+var hero=document.getElementById("hero");
+hero.style.background="url('"+topBanner[random]+"') top center";
+
 }
 
 var brand="AKM IT SOLUTION CENTRE";
@@ -21,3 +34,4 @@ function Msgfield(e){
 	e.style.height="1px"
 	e.style.height=(e.scrollHeight)+"px";
 }
+
